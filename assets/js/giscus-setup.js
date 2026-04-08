2ee86335b92006c9501b6836ae1a23feeec8091c
Qinghao-Guan <104255358+Qinghao-Guan@users.noreply.github.com>
2026-04-08 13:54:00 +0000

    Deploying to gh-pages from @ Qinghao-Guan/qinghao-guan.github.io@ff819a3bb7a4923af25fceb782862dece3ec0442 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Qinghao-Guan/qinghao.github.io",
+    "data-repo": "Qinghao-Guan/qinghao-guan.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
