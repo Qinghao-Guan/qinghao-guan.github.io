30f71a8487491b82e6f455094a0e2b23e80c532e
Qinghao-Guan <104255358+Qinghao-Guan@users.noreply.github.com>
2026-04-10 13:29:36 +0000

    Deploying to gh-pages from @ Qinghao-Guan/qinghao-guan.github.io@e611587414858fd15c8927674b5afd556d8008b0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -420,18 +420,11 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%71.%67%75%61%6E@%69%6B%6D%7A.%75%7A%68.%63%68", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -452,14 +445,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=50wF_BAAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
